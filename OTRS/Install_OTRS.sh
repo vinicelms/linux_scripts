38a252dcd2782d31b334626ecd18d480899799a3
Vinicius Celms <by.vinicius@gmail.com>
2016-08-20 14:16:50 -0400

    First structure in script

---
+++ OTRS/Install_OTRS.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Script to download and install OTRS with all dependecies
+# This script is designed to be used on Debian (8.5 x64)
+# Created by: Vinicius Celms
+# Date: 20/08/2016
+# Version: 1.0
+# 
+
