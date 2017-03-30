657dd39265e417410ee46b865b49cd8f558adc64
Vinicius Celms <by.vinicius@gmail.com>
2017-03-30 14:31:59 -0300

    MediaWiki installer version one

--- MediaWiki/MediaWiki_Install.sh
+++ MediaWiki/MediaWiki_Install.sh
@@ -19,6 +19,9 @@ LINK_MEDIAWIKI_DOWNLOAD='https://releases.wikimedia.org/mediawiki/1.28/mediawiki
 MEDIAWIKI_NAME=$(echo $LINK_MEDIAWIKI_DOWNLOAD | cut -d/ -f$(expr $(echo $LINK_MEDIAWIKI_DOWNLOAD | grep -o '/' | wc -l ) + 1))
 MEDIAWIKI_STATIC_DIRECTORY=/var/lib/mediawiki
 
+UPLOAD_FILESIZE_PARAM=20M
+MEMORY_LIMIT_PARAM=128M
+
 # Install requirements
 apt-get install apache2 postgresql php5 php5-pgsql libapache2-mod-php5 php5-apcu imagemagick php5-intl
 if [ $? -eq 0 ]
@@ -37,7 +40,7 @@ if [ $? -eq 0 ]
 		PrintMessage "Inflate files: Error"
 fi
 
-# Move files to /var/lib/mediawiki ($MEDIAWIKI_STATIC_DIRECTORY)
+# Move files MediaWiki directory ($MEDIAWIKI_STATIC_DIRECTORY)
 mv $MEDIAWIKI_NAME $MEDIAWIKI_STATIC_DIRECTORY
 if [ $? -eq 0 ]
 	then
@@ -47,3 +50,56 @@ if [ $? -eq 0 ]
 fi
 
 # Change configuration on 'php.ini'
+
+# Get upload_max_filesize line number
+UPLOADFILESIZE_LINE=$(awk '/upload_max_filesize/{ print NR; exit}' /etc/php5/apache2/php.ini)
+
+# Get upload_max_filesize value
+UPLOADFILESIZE_INFO=$(sed $UPLOADFILESIZE_LINE"q;d" /etc/php5/apache2/php.ini)
+UPLOADFILESIZE_INFO=$(echo ${UPLOADFILESIZE_INFO: -3})
+
+# Set value to upload_max_filesize
+UPLOADFILESIZE_INFO=$UPLOADFILESIZE_LINE"s/"$UPLOADFILESIZE_INFO"${UPLOAD_FILESIZE_PARAM}"
+sed -i "$(echo $UPLOADFILESIZE_INFO)" /etc/php5/apache2/php.ini
+if [ $? -eq 0 ]
+	then
+		PrintMessage "Change parameter 'upload_max_filesize' to ${UPLOAD_FILESIZE_PARAM}: Success"
+	else
+		PrintMessage "Change parameter 'upload_max_filesize' to ${UPLOAD_FILESIZE_PARAM}: Error"
+fi
+
+# Get memory_limit line number
+MEMORYLIMIT_LINE=$(awk '/memory_limit/{ print NR; exit}' /etc/php5/apache2/php.ini)
+
+# Get memory_limit value
+MEMORYLIMIT_INFO=$(sed $MEMORYLIMIT_LINE"q;d" /etc/php5/apache2/php.ini)
+MEMORYLIMIT_INFO=$(echo ${MEMORYLIMIT_INFO: -3})
+
+# Set value to memory_limit
+MEMORYLIMIT_INFO=$MEMORYLIMIT_LINE"s/"$MEMORYLIMIT_INFO"${MEMORY_LIMIT_PARAM}"
+sed -i "$(echo $MEMORYLIMIT_INFO)" /etc/php5/apache2/php.ini
+if [ $? -eq 0 ]
+	then
+		PrintMessage "Change parameter 'memory_limit' to ${MEMORY_LIMIT_PARAM}: Success"
+	else
+		PrintMessage "Change parameter 'memory_limit' to ${MEMORY_LIMIT_PARAM}: Error"
+fi
+
+# Create symbolic link from MediaWiki directory ($MEDIAWIKI_STATIC_DIRECTORY)
+cd /var/www/html
+ln -s $MEDIAWIKI_STATIC_DIRECTORY $(echo $MEDIAWIKI_STATIC_DIRECTORY | cut -d/ -f$(expr $(echo $MEDIAWIKI_STATIC_DIRECTORY | grep -o '/' | wc -l ) + 1))
+if [ $? -eq 0 ]
+	then
+		PrintMessage "Create symbolic link from MediaWiki: Success"
+	else
+		PrintMessage "Create symbolic link from MediaWiki: Error"
+fi
+
+# Delete downloaded file
+rm -rf $MEDIAWIKI_NAME
+if [ $? -eq 0 ]
+	then
+		PrintMessage "Delete downloaded file: Success"
+	else
+		PrintMessage "Delete downloaded file: Error"
+fi
